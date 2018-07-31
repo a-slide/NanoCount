ed737d7c9594339d7e2287fda951d7b7e818d60c
a-slide <aleg@ebi.ac.uk>
2018-07-31 16:55:17 +0100

    remove long descr

--- NanoCount/__init__.py
+++ NanoCount/__init__.py
@@ -5,14 +5,12 @@ __version__ = "0.1.a2"
 __all__ = ["NanoCount", "Read"]
 
 description = 'EM based transcript abundance from nanopore reads mapped to a transcriptome with minimap2'
-long_description = """"""
 
 # Collect info in a dictionary for setup.py
 setup_dict = {
     "name": __name__,
     "version": __version__,
     "description": description,
-    "long_description": long_description,
     "url": "https://github.com/a-slide/NanoCount",
     "author": 'Adrien Leger',
     "author_email": 'aleg {at} ebi.ac.uk',
