3bc8350f57651fb57173a32b84c5676222ef56b5
a-slide <aleg@ebi.ac.uk>
2020-05-04 22:22:07 +0100

    Update version and packaging information

--- setup.py
+++ setup.py
@@ -4,16 +4,25 @@
 from setuptools import setup
 import NanoCount as package
 
+# Define package info
+name = "NanoCount"
+version = "0.2.0"
+description = "EM based transcripts abundance estimation from nanopore reads mapped to a transcriptome with minimap2"
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 # Collect info in a dictionary for setup.py
 setup(
-    name = package.__name__,
-    version = package.__version__,
-    description = package.__description__,
+    name = name,
+    description = description,
+    version = version,
+    long_description = long_description,
+    long_description_content_type="text/markdown",
     url = "https://github.com/a-slide/NanoCount",
     author = 'Adrien Leger',
     author_email = 'aleg@ebi.ac.uk',
     license = "MIT",
-    python_requires ='>=3.5',
+    python_requires ='>=3.6',
     classifiers = [
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Science/Research',
@@ -21,7 +30,9 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3'],
     install_requires = [
-        'pysam>=0.14.1',
-        'pandas>=0.23.3'],
-    packages = [package.__name__],
+        'pysam>=0.15.4',
+        'pandas>=1.0.3',
+        'tqdm>=4.46.0',
+        'colorlog>=4.1.0'],
+    packages = [name],
     entry_points = {'console_scripts': ['NanoCount = NanoCount.__main__:main']})
