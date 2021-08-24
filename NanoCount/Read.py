87d8270acc356788c0617e3c7693c1732d739373
a-slide <adrien.leger@nanoporetech.com>
2021-08-24 09:44:46 +0100

    Fix for secondary reads without sequence

--- NanoCount/Read.py
+++ NanoCount/Read.py
@@ -39,7 +39,11 @@ class Read(object):
 
     def get_secondary_alignments_list(self, primary_score="primary"):
         primary_alignment_idx = self._get_primary_idx(primary_score=primary_score)
-        return [alignment for i, alignment in enumerate(self.alignment_list) if i != primary_alignment_idx]
+        return [
+            alignment
+            for i, alignment in enumerate(self.alignment_list)
+            if i != primary_alignment_idx
+        ]
 
     def add_pysam_alignment(self, pysam_aligned_segment, read_idx, **kwargs):
         self.alignment_list.append(Alignment(pysam_aligned_segment, read_idx))
@@ -80,15 +84,26 @@ class Alignment:
         """"""
         self.qname = pysam_aligned_segment.query_name
         self.rname = pysam_aligned_segment.reference_name
-        self.qlen = int(pysam_aligned_segment.query_length)
+        qlen = int(pysam_aligned_segment.query_length)
+        if qlen == 0:
+            self.qlen = int(pysam_aligned_segment.infer_query_length())
+        else:
+            self.qlen = qlen
         self.align_len = int(pysam_aligned_segment.query_alignment_length)
         self.align_score = int(pysam_aligned_segment.get_tag("AS"))
-        self.secondary = pysam_aligned_segment.is_secondary or pysam_aligned_segment.is_supplementary
+        self.secondary = (
+            pysam_aligned_segment.is_secondary or pysam_aligned_segment.is_supplementary
+        )
         self.read_idx = read_idx
 
     def __repr__(self):
         return "Query:{} | Reference:{} | Query len:{} | Alignment len:{} | Align Score:{} | Secondary:{}".format(
-            self.qname, self.rname, self.qlen, self.align_len, self.align_score, self.secondary
+            self.qname,
+            self.rname,
+            self.qlen,
+            self.align_len,
+            self.align_score,
+            self.secondary,
         )
 
     # ~~~~~~~~~~~~~~PROPERTY METHODS~~~~~~~~~~~~~~ #
