9d762b50bf114ec38efb51f5c0e8955707caf4a8
a-slide <adrien.leger@nanoporetech.com>
2021-09-08 23:15:56 +0100

    minor change to doc

--- NanoCount/__init__.py
+++ NanoCount/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __name__ = "NanoCount"
-__version__ = "0.4.0"
+__version__ = "0.4.0.post1"
 __description__ = "NanoCount estimates transcripts abundance from Oxford Nanopore *direct-RNA sequencing* datasets, using an expectation-maximization approach like RSEM, Kallisto, salmon, etc to handle the uncertainty of multi-mapping reads"
 __url__ = "https://github.com/a-slide/NanoCount/"
 __licence__ = "MIT"
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NanoCount v0.4.0
+# NanoCount v0.4.0.post1
 
 ![NanoCount](./docs/pictures/NanoCount.png)
 
